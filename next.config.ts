a0788915d28da8676b28c922727517561b5f2f5e
Atul Chandra Nath <enggatulbd@gmail.com>
2026-01-28 01:31:20 +0100

    fixing firebase errors

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,18 @@
 import type { NextConfig } from "next";
 
+const repoName = 'career-builder'; // Change to your repo name if different
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: "export",
+  assetPrefix: `/${repoName}/`,
+  basePath: `/${repoName}`,
   reactStrictMode: true,
   images: {
-    domains: ['firebasestorage.googleapis.com'],
+    domains: ["firebasestorage.googleapis.com"],
     unoptimized: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
 };
 
 export default nextConfig;
